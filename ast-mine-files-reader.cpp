5d340d4dc6eafc7f1871bba28a0b3651d7ab122d
mike yang <18604713262@163.com>
2021-10-12 15:08:14 -0400

    added project dynamic function reader

--- ast-mine-files-reader.cpp
+++ ast-mine-files-reader.cpp
@@ -2,8 +2,39 @@
 #include <boost/property_tree/json_parser.hpp>
 #include <map>
 #include <iostream>
+#include <string>
+#include <boost/optional/optional.hpp>
+#include<boost/algorithm/string.hpp>
+
+using namespace std;
+namespace alg = boost::algorithm;
 
 namespace pt = boost::property_tree;
+
+string get_three(string a){
+  alg::ireplace_all(a,".","#");
+  vector<string> strs;
+
+  boost::split(strs,a,boost::is_any_of("/"));
+  if(strs.size()<3)return "";
+  string elem_name = strs[0]+"."+strs[1]+"."+strs[2];
+  
+  return elem_name;
+
+}
+
+void proj_freq(pt::ptree &out_tree, string a,long int freqc){
+  boost::optional<  pt::ptree& > child = out_tree.get_child_optional( "a" );
+  if( !child )
+  {
+    out_tree.put(a,freqc);
+  }
+  else {
+    long int v = out_tree.get<long int>(a);
+    v +=freqc;
+    out_tree.put(a,v);
+  }
+}
 
 int main (int argc, char ** argv){
   if (argc != 2) {
@@ -14,24 +45,28 @@ int main (int argc, char ** argv){
     std::ifstream jsonFile(argv[1]);
 
     pt::ptree propt;
-    long int count = 0, count_file = 0;
+    pt::ptree out_tree;
+    long int count = 0, count_file = 0, count_proj = 0;
     pt::read_json(jsonFile, propt);
     for (auto & property: propt) {
       std::cout << property.first << "----\n";
-      for (auto & elem:property.second){
-        // std::cout<< "\t"<<elem.first<<"\n\t";
-        // std::cout<<elem.second.get_value<std::string>();
+      for (auto & elem:property.second){//file occurence
+        string elem_name = elem.first;
+        elem_name = get_three(elem_name);
         count_file++;
-        for (auto & property: elem.second) {
+        long int file_freq =0;
+        for (auto & property: elem.second) {//occurence line number
           count++;
-            // std::cout <<" "<<property.second.get_value < std::string > ();
+          file_freq++;
         }
-        // std::cout<<std::endl;
+        proj_freq(out_tree,property.first+"."+elem_name,file_freq);
+        // out_tree.put(property.first+"."+elem_name,file_freq);
       }
       std::cout << "\t-"<< count<<" occurences in "<<count_file<<" files" << "\n";
       count = 0;
       count_file = 0;
     }
+    write_json("reader_out.json",out_tree);
   }
   return 0;
   
