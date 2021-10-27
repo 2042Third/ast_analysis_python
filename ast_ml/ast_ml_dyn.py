d91b409d6642a17adbe65eafcbe51a567239f433
mike yang <18604713262@163.com>
2021-10-27 12:37:33 -0400

    Seperated files

--- ast_ml/ast_ml_dyn.py
+++ ast_ml/ast_ml_dyn.py
@@ -3,7 +3,7 @@
   {
    "cell_type": "code",
    "execution_count": 1,
-   "id": "2d0076c7",
+   "id": "a39780d1",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -22,7 +22,7 @@
   {
    "cell_type": "code",
    "execution_count": 2,
-   "id": "5d68f5ff",
+   "id": "618e471e",
    "metadata": {},
    "outputs": [],
    "source": [
@@ -32,40 +32,71 @@
   },
   {
    "cell_type": "markdown",
-   "id": "e530831f",
+   "id": "962a5699",
    "metadata": {},
    "source": [
-    "## Decorators and Functional"
+    "## Functional and Dynamic"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": 3,
-   "id": "418e9a76",
+   "id": "8ab09bee",
    "metadata": {},
    "outputs": [],
    "source": [
-    "trainxor = ad[['Dec', \"Func\"]]\n",
+    "trainxor = ad[['Dyn', \"Func\"]]\n",
     "trainyor = ad[['Relatedness']]\n",
-    "# trainx = ad[['Dyn']]\n",
-    "# trainy = ad[[ \"Func\", 'DynFunc']]"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 4,
-   "id": "71f5ddcb",
-   "metadata": {},
-   "outputs": [],
-   "source": [
     "X_train, X_test, y_train, y_test = train_test_split(\n",
     "    trainxor, trainyor, test_size=0.1, random_state=0)"
    ]
   },
   {
+   "cell_type": "markdown",
+   "id": "4d6e17b2",
+   "metadata": {},
+   "source": [
+    "### Bayesian"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 4,
+   "id": "a19de907",
+   "metadata": {},
+   "outputs": [
+    {
+     "name": "stdout",
+     "output_type": "stream",
+     "text": [
+      "[0.65181384 0.65168534 0.6553915  0.65936667 0.6712922  0.68321772\n",
+      " 0.69116807]\n"
+     ]
+    }
+   ],
+   "source": [
+    "from sklearn import linear_model\n",
+    "\n",
+    "reg = linear_model.BayesianRidge()\n",
+    "dn, fc = 0, 3\n",
+    "model = reg.fit(X_train, y_train.values.ravel())\n",
+    "xnew = [[0,1],[1,0],[0,10],[0,20],[0,50],[0,80],[0,100]]\n",
+    "y_pred = model.predict(xnew)\n",
+    "print(y_pred)"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "7f3a6707",
+   "metadata": {},
+   "source": [
+    "### Gaussian "
+   ]
+  },
+  {
    "cell_type": "code",
    "execution_count": 5,
-   "id": "c2bced5e",
+   "id": "54cb69a5",
    "metadata": {},
    "outputs": [
     {
@@ -74,28 +105,27 @@
      "text": [
       "              precision    recall  f1-score   support\n",
       "\n",
-      "           0       0.75      0.99      0.85       978\n",
-      "           1       1.00      0.83      0.91      1980\n",
+      "           0       0.67      0.99      0.80       978\n",
+      "           1       1.00      0.76      0.86      1980\n",
       "\n",
-      "    accuracy                           0.89      2958\n",
-      "   macro avg       0.87      0.91      0.88      2958\n",
-      "weighted avg       0.91      0.89      0.89      2958\n",
+      "    accuracy                           0.83      2958\n",
+      "   macro avg       0.83      0.87      0.83      2958\n",
+      "weighted avg       0.89      0.83      0.84      2958\n",
       "\n",
-      "[[ 972    6]\n",
-      " [ 332 1648]]\n",
-      "Precision score: 0.9963724304715841\n",
-      "X=[0, 1], Predicted=[0.0, 1.0]\n",
-      "X=[1, 0], Predicted=[1.0, 0.0]\n",
+      "[[ 971    7]\n",
+      " [ 482 1498]]\n",
+      "Precision score: 0.9953488372093023\n",
+      "X=[0, 1], Predicted=[1.0, 0.0]\n",
+      "X=[1, 0], Predicted=[0.0, 1.0]\n",
       "X=[0, 10], Predicted=[1.0, 0.0]\n",
       "X=[0, 20], Predicted=[1.0, 0.0]\n",
-      "X=[0, 50], Predicted=[0.0, 1.0]\n",
-      "X=[0, 80], Predicted=[0.0, 1.0]\n",
+      "X=[0, 50], Predicted=[1.0, 0.0]\n",
+      "X=[0, 80], Predicted=[1.0, 0.0]\n",
       "X=[0, 100], Predicted=[0.0, 1.0]\n"
      ]
     }
    ],
    "source": [
-    "\n",
     "gnb = GaussianNB()\n",
     "dn, fc = 0, 3\n",
     "model = gnb.fit(X_train, y_train.values.ravel())\n",
@@ -106,17 +136,21 @@
     "xnew = [[0,1],[1,0],[0,10],[0,20],[0,50],[0,80],[0,100]]\n",
     "xprobs = model.predict_proba(X_test)\n",
     "for i in range(len(xnew)):\n",
-    "\tprint(\"X=%s, Predicted=%s\" % (xnew[i], [round(xprobs[i][0],3),round(xprobs[i][1],3)]))\n",
-    "# while True:\n",
-    "#   dn, fc = [int(x) for x in input(\"Next prediction\\n\").split(',')]\n",
-    "#   y_pred = model.predict([[dn, fc]])\n",
-    "#   print(\"Predict [%d, %d] %d\" % (dn, fc, y_pred))"
+    "\tprint(\"X=%s, Predicted=%s\" % (xnew[i], [round(xprobs[i][0],3),round(xprobs[i][1],3)]))"
+   ]
+  },
+  {
+   "cell_type": "markdown",
+   "id": "7421decb",
+   "metadata": {},
+   "source": [
+    "### Logistic Regression"
    ]
   },
   {
    "cell_type": "code",
    "execution_count": 6,
-   "id": "14d7c05c",
+   "id": "aee6389e",
    "metadata": {},
    "outputs": [
     {
@@ -125,22 +159,22 @@
      "text": [
       "              precision    recall  f1-score   support\n",
       "\n",
-      "           0       0.75      0.99      0.85       978\n",
-      "           1       0.99      0.84      0.91      1980\n",
+      "           0       0.68      0.98      0.80       978\n",
+      "           1       0.99      0.77      0.86      1980\n",
       "\n",
-      "    accuracy                           0.89      2958\n",
-      "   macro avg       0.87      0.91      0.88      2958\n",
-      "weighted avg       0.91      0.89      0.89      2958\n",
+      "    accuracy                           0.84      2958\n",
+      "   macro avg       0.83      0.87      0.83      2958\n",
+      "weighted avg       0.88      0.84      0.84      2958\n",
       "\n",
-      "[[ 968   10]\n",
-      " [ 320 1660]]\n",
-      "Precision score: 0.9940119760479041\n",
-      "X=[0, 1], Predicted=[0.0, 1.0]\n",
-      "X=[1, 0], Predicted=[0.746, 0.254]\n",
-      "X=[0, 10], Predicted=[0.766, 0.234]\n",
-      "X=[0, 20], Predicted=[0.754, 0.246]\n",
-      "X=[0, 50], Predicted=[0.0, 1.0]\n",
-      "X=[0, 80], Predicted=[0.0, 1.0]\n",
+      "[[ 958   20]\n",
+      " [ 457 1523]]\n",
+      "Precision score: 0.9870382372002592\n",
+      "X=[0, 1], Predicted=[0.648, 0.352]\n",
+      "X=[1, 0], Predicted=[0.0, 1.0]\n",
+      "X=[0, 10], Predicted=[0.706, 0.294]\n",
+      "X=[0, 20], Predicted=[0.692, 0.308]\n",
+      "X=[0, 50], Predicted=[0.702, 0.298]\n",
+      "X=[0, 80], Predicted=[0.711, 0.289]\n",
       "X=[0, 100], Predicted=[0.0, 1.0]\n"
      ]
     }
@@ -160,53 +194,17 @@
    ]
   },
   {
+   "cell_type": "markdown",
+   "id": "103d9686",
+   "metadata": {},
+   "source": [
+    "### Neural Network"
+   ]
+  },
+  {
    "cell_type": "code",
    "execution_count": 7,
-   "id": "5fbc6090",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "[0.65208472 0.65187702 0.65538378 0.6590494  0.67004626 0.68104313\n",
-      " 0.68837437]\n"
-     ]
-    }
-   ],
-   "source": [
-    "from sklearn import linear_model\n",
-    "\n",
-    "reg = linear_model.BayesianRidge()\n",
-    "dn, fc = 0, 3\n",
-    "model = reg.fit(X_train, y_train.values.ravel())\n",
-    "xnew = [[0,1],[1,0],[0,10],[0,20],[0,50],[0,80],[0,100]]\n",
-    "y_pred = model.predict(xnew)\n",
-    "print(y_pred)\n",
-    "# print(classification_report(y_test,y_pred))\n",
-    "# print(confusion_matrix(y_test, y_pred))\n",
-    "# print(\"Precision score: {}\".format(precision_score(y_test,y_pred)))\n",
-    "# xprobs = model.predict_proba(X_test)\n",
-    "# for i in range(len(xnew)):\n",
-    "# \tprint(\"X=%s, Predicted=%s\" % (xnew[i], [round(xprobs[i][0],3),round(xprobs[i][1],3)]))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 8,
-   "id": "abf2fed6",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "mlpc=MLPClassifier(hidden_layer_sizes=(11,11,11),max_iter=500)\n",
-    "model = mlpc.fit(X_train, y_train.values.ravel())\n",
-    "y_pred = model.predict(X_test)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 9,
-   "id": "205c509f",
+   "id": "3e740052",
    "metadata": {},
    "outputs": [
     {
@@ -215,27 +213,30 @@
      "text": [
       "              precision    recall  f1-score   support\n",
       "\n",
-      "           0       0.76      0.97      0.85       978\n",
-      "           1       0.98      0.85      0.91      1980\n",
+      "           0       0.71      0.85      0.77       978\n",
+      "           1       0.92      0.83      0.87      1980\n",
       "\n",
-      "    accuracy                           0.89      2958\n",
-      "   macro avg       0.87      0.91      0.88      2958\n",
-      "weighted avg       0.91      0.89      0.89      2958\n",
+      "    accuracy                           0.84      2958\n",
+      "   macro avg       0.81      0.84      0.82      2958\n",
+      "weighted avg       0.85      0.84      0.84      2958\n",
       "\n",
-      "[[ 950   28]\n",
-      " [ 306 1674]]\n",
-      "Precision score: 0.9835487661574618\n",
-      "X=[0, 1], Predicted=[0.0, 1.0]\n",
-      "X=[1, 0], Predicted=[0.701, 0.299]\n",
-      "X=[0, 10], Predicted=[0.802, 0.198]\n",
-      "X=[0, 20], Predicted=[0.726, 0.274]\n",
-      "X=[0, 50], Predicted=[0.0, 1.0]\n",
-      "X=[0, 80], Predicted=[0.0, 1.0]\n",
+      "[[ 836  142]\n",
+      " [ 344 1636]]\n",
+      "Precision score: 0.9201349831271091\n",
+      "X=[0, 1], Predicted=[0.493, 0.507]\n",
+      "X=[1, 0], Predicted=[0.0, 1.0]\n",
+      "X=[0, 10], Predicted=[0.724, 0.276]\n",
+      "X=[0, 20], Predicted=[0.615, 0.385]\n",
+      "X=[0, 50], Predicted=[0.69, 0.31]\n",
+      "X=[0, 80], Predicted=[0.755, 0.245]\n",
       "X=[0, 100], Predicted=[0.0, 1.0]\n"
      ]
     }
    ],
    "source": [
+    "mlpc=MLPClassifier(hidden_layer_sizes=(11,11,11),max_iter=500)\n",
+    "model = mlpc.fit(X_train, y_train.values.ravel())\n",
+    "y_pred = model.predict(X_test)\n",
     "print(classification_report(y_test,y_pred))\n",
     "print(confusion_matrix(y_test, y_pred))\n",
     "print(\"Precision score: {}\".format(precision_score(y_test,y_pred)))\n",
@@ -246,15 +247,17 @@
    ]
   },
   {
-   "cell_type": "markdown",
-   "id": "4433df9c",
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "875e4d81",
    "metadata": {},
+   "outputs": [],
    "source": []
   },
   {
    "cell_type": "code",
    "execution_count": null,
-   "id": "fd633b41",
+   "id": "80cc15c4",
    "metadata": {},
    "outputs": [],
    "source": []
